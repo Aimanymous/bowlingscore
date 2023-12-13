80fc8c032b07b5278922c6d7e1bdd1b60b22fc3b
Aiman Ahmad <86181309+Aimanymous@users.noreply.github.com>
2023-12-14 00:39:45 +0800

    Add files via upload

---
+++ bowling.cpp
@@ -0,0 +1,255 @@
+#include <iostream>
+using namespace std;
+
+const int MAX_FRAMES = 10;
+
+enum single { Strike = 10, Spare };
+
+struct Frame {
+    int firstBowl;
+    int secondBowl;
+    int thirdBowl;
+};
+
+void getFrames(Frame[]);
+int calcScore(Frame[]);
+
+int main() {
+    Frame frames[MAX_FRAMES];
+
+    getFrames(frames);
+    calcScore(frames);
+    system("pause");
+    return 0;
+}
+
+void getFrames(Frame frames[]) {
+    char temp;
+
+    for (int i = 0; i < MAX_FRAMES; i++) {
+        int pins = 10;
+        if (i < 9) {
+            cout << "Input first bowl for frame " << i + 1 << ":" << endl;
+            cin >> temp;
+            if (temp == 'x' || temp == 'X') {
+                frames[i].firstBowl = Strike;
+            }
+            else {
+                frames[i].firstBowl = (int)(temp - '0');
+                pins = 10 - frames[i].firstBowl;
+                cout << "Input second bowl for frame " << i + 1 << ":" << endl;
+                cin >> temp;
+                if (temp == '/') {
+                    frames[i].secondBowl = Spare;
+                    frames[i].thirdBowl = 0;
+                }
+                else if ((int)(temp - '0') >= pins) {
+                    frames[i].secondBowl = Spare;
+                    frames[i].thirdBowl = 0;
+                }
+                else {
+                    frames[i].secondBowl = (int)(temp - '0');
+                    frames[i].thirdBowl = 0;
+                }
+            }
+        }
+        else {
+            cout << "Input first bowl for final frame:" << endl;
+            cin >> temp;
+            if (temp == 'x' || temp == 'X') {
+                frames[i].firstBowl = Strike;
+                pins = 10;
+                cout << "Input second bowl for final frame (Can't be '/'):" << endl;
+                cin >> temp;
+                if (temp == 'x' || temp == 'X') {
+                    frames[i].secondBowl = Strike;
+                }
+                else {
+                    frames[i].secondBowl = (int)(temp - '0');
+                    pins = 10 - frames[i].secondBowl;
+                }
+                cout << "Input third bowl for final frame:" << endl;
+                cin >> temp;
+                if (temp == 'x' || temp == 'X') {
+                    frames[i].thirdBowl = Strike;
+                }
+                else if (temp == '/') {
+                    frames[i].thirdBowl = Spare;
+                }
+                else if ((int)(temp - '0') >= pins) {
+                    frames[i].thirdBowl = Spare;
+                }
+                else {
+                    frames[i].thirdBowl = (int)(temp - '0');
+                }
+            }
+            else {
+                frames[i].firstBowl = (int)(temp - '0');
+                pins = 10 - frames[i].firstBowl;
+                cout << "Input second bowl for final frame:" << endl;
+                cin >> temp;
+                if (temp == '/') {
+                    frames[i].secondBowl = Spare;
+                    cout << "Input third bowl for final frame:" << endl;
+                    cin >> temp;
+                    if (temp == 'x' || temp == 'X') {
+                        frames[i].thirdBowl = Strike;
+                    }
+                    else {
+                        frames[i].thirdBowl = (int)(temp - '0');
+                    }
+                }
+                else if ((int)(temp - '0') >= pins) {
+                    frames[i].secondBowl = Spare;
+                    cout << "Input third bowl for final frame:" << endl;
+                    cin >> temp;
+                    if (temp == 'x' || temp == 'X') {
+                        frames[i].thirdBowl = Strike;
+                    }
+                    else {
+                        frames[i].thirdBowl = (int)(temp - '0');
+                    }
+                }
+                else {
+                    frames[i].secondBowl = (int)(temp - '0');
+                    frames[i].thirdBowl = 0;
+                }
+            }
+        }
+    }
+
+    // Display Overall Frames
+    cout << "Overall Frames:" << endl;
+    for (int z = 1; z <= MAX_FRAMES; z++) {
+        cout << "Frame " << z << "\t";
+    }
+    cout << endl;
+    for (int k = 0; k < MAX_FRAMES; k++) {
+        if (k < 9) {
+            if (frames[k].firstBowl == Strike) {
+                cout << "X" << "\t";
+            }
+            else {
+                cout << frames[k].firstBowl << " ";
+                if (frames[k].secondBowl == Spare) {
+                    cout << "/" << "\t";
+                }
+                else {
+                    cout << frames[k].secondBowl << "\t";
+                }
+            }
+        }
+        else {
+            if (frames[k].firstBowl == Strike) {
+                cout << "X" << " ";
+            }
+            else {
+                cout << frames[k].firstBowl << " ";
+                if (frames[k].secondBowl == Spare) {
+                    if (frames[k].thirdBowl == Strike) {
+                        cout << "/" << " " << frames[k].thirdBowl << endl;
+                    }
+                    else {
+                        cout << "/" << frames[k].thirdBowl << endl;
+                    }
+                }
+                else {
+                    cout << frames[k].secondBowl << endl;
+                }
+            }
+            if (frames[k].secondBowl == Strike) {
+                cout << "X" << " ";
+            }
+            else {
+                cout << frames[k].secondBowl << " ";
+                if (frames[k].thirdBowl == Spare) {
+                    cout << "/" << endl;
+                }
+                else {
+                    cout << frames[k].thirdBowl << endl;
+                }
+            }
+            if (frames[k].thirdBowl == Strike) {
+                cout << "X" << endl;
+            }
+        }
+    }
+}
+
+int calcScore(Frame frames[]) {
+    int total = 0;
+    int frameTotal[MAX_FRAMES];
+    for (int i = 0; i < MAX_FRAMES; i++) {
+        if (i < 8) {
+            if (frames[i].firstBowl == Strike) {
+                if (frames[i + 1].firstBowl == Strike) {
+                    frameTotal[i] = frames[i].firstBowl + frames[i + 1].firstBowl + frames[i + 2].firstBowl;
+                    total = total + frameTotal[i];
+                }
+                else if (frames[i + 1].secondBowl == Spare) {
+                    frameTotal[i] = frames[i].firstBowl + 10;
+                    total = total + frameTotal[i];
+                }
+                else {
+                    frameTotal[i] = frames[i].firstBowl + frames[i + 1].firstBowl + frames[i + 1].secondBowl;
+                    total = total + frameTotal[i];
+                }
+            }
+            else if (frames[i].secondBowl == Spare) {
+                frameTotal[i] = 10 + frames[i + 1].firstBowl;
+                total = total + frameTotal[i];
+            }
+            else {
+                frameTotal[i] = frames[i].firstBowl + frames[i].secondBowl;
+                total = total + frameTotal[i];
+            }
+        }
+        else if (i == 8) {
+            if (frames[i].firstBowl == Strike) {
+                if (frames[i + 1].secondBowl == Spare) {
+                    frameTotal[i] = frames[i].firstBowl + 10;
+                    total = total + frameTotal[i];
+                }
+                else {
+                    frameTotal[i] = frames[i].firstBowl + frames[i + 1].firstBowl + frames[i + 1].secondBowl;
+                    total = total + frameTotal[i];
+                }
+            }
+            else if (frames[i].secondBowl == Spare) {
+                frameTotal[i] = frames[i + 1].firstBowl + 10;
+                total = total + frameTotal[i];
+            }
+            else {
+                frameTotal[i] = frames[i].firstBowl + frames[i].secondBowl;
+                total = total + frameTotal[i];
+            }
+        }
+        else {
+            if (frames[i].firstBowl == Strike) {
+                if (frames[i].thirdBowl == Spare) {
+                    frameTotal[i] = 20;
+                    total = total + frameTotal[i];
+                }
+                else if (frames[i].thirdBowl == Strike) {
+                    frameTotal[i] = 30;
+                    total = total + frameTotal[i];
+                }
+                else {
+                    frameTotal[i] = 10 + frames[i].secondBowl + frames[i].thirdBowl;
+                    total = total + frameTotal[i];
+                }
+            }
+            else if (frames[i].secondBowl == Spare) {
+                frameTotal[i] = 10 + frames[i].thirdBowl;
+                total = total + frameTotal[i];
+            }
+            else {
+                frameTotal[i] = frames[i].firstBowl + frames[i].secondBowl;
+                total = total + frameTotal[i];
+            }
+        }
+        cout << total << "\t";
+    }
+    cout << endl;
+    return total;
+}
